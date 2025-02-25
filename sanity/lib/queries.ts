635f8f88c941942d2e16b073f4f262f7848f0c22
affenschlabi00 <steam.keanu@gmail.com>
2025-02-25 14:45:28 +0100

    implement queries for api calls

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,18 +1,34 @@
 import {defineQuery} from "groq";
 
-export const POSTS_QUERY =
-    defineQuery(
-        `*[_type == "post"]{
-          title,
-          description,
-          views,
-          files[]{
-            asset->{
-              _id,
-              url,
-              originalFilename,
-              mimeType
+export const POSTS_QUERY = (page: number, limit: number) => {
+    const start = (page - 1) * limit; // Calculate the starting point for the current page
+    const end = start + limit; // Calculate the endpoint for the current page
+
+    return defineQuery(
+        `*[_type == "post"] | order(_createdAt desc) [${start}...${end}] {
+            title,
+            description,
+            views,
+            preview {
+                asset->{
+                    url,
+                    originalFilename,
+                    mimeType
+                }
+            },
+            files[]{
+                asset->{
+                    _id,
+                    url,
+                    originalFilename,
+                    mimeType
+                }
             }
-          }
         }`
     );
+};
+
+
+export const TOTAL_POSTS_COUNT_QUERY = defineQuery(
+    `count(*[_type == "post"])`
+);
