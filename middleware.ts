c3b52ecd9e50211da645afad7a413f655ada8cbe
affenschlabi00 <steam.keanu@gmail.com>
2025-02-28 18:33:20 +0100

    fix any type error in request

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 const globalPassPhrase = process.env.NEXT_PUBLIC_PASS_PHRASE;
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
 
     const url = req.nextUrl.pathname;
+
+    if (typeof globalPassPhrase === 'undefined') {
+        return NextResponse.redirect(new URL('/access', req.url));
+    }
 
     if (!req.cookies.has(globalPassPhrase) && url !== '/access') {
         return NextResponse.redirect(new URL('/access', req.url));
